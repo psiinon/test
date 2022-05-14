7717bec464632256ae4279e29a2a5311725b79b5
Simon Bennetts <psiinon@gmail.com>
2022-05-14 09:51:40 +0100

    Update ssti-score-main.js

--- ssti/ssti-score-main.js
+++ ssti/ssti-score-main.js
@@ -1,8 +1,7 @@
 // This script will _not_ run on its own - it needs a set of fields prepended to it
 //var DIR = "/zap/wrk/";
 var YAML_FILE = "/zap/wrk/all.yml";
-//var RULES = [40012, 40026, 90019, 90025, 90035, 90036];
-var RULES = [40012, 40026, 90019, 90025];
+var RULES = [40012, 40026, 90019, 90025, 90035, 90036];
 var target = 'http://localhost:' + PORT + '/';
   
 var FileWriter = Java.type('java.io.FileWriter');
